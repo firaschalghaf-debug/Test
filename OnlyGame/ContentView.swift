14d85e6fbca2d5078b17c9303009e2aaa7a893e1
Preuttipong Rimchaisith <maxxysnap@gmail.com>
2026-03-31 13:41:52 +0700

    Initial Commit

---
+++ OnlyGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnlyGame
+//
+//  Created by Max on 31/3/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnlyGame/OnlyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlyGameApp.swift
+//  OnlyGame
+//
+//  Created by Max on 31/3/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct OnlyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
